96c1bfd6a68744b6afd54e0e46ea46d158382867
Kelly Mills <ervinkellym@gmail.com>
2024-11-22 16:49:22 -0500

    Update styling with new font and more muted colors

--- src/TabBar.jsx
+++ src/TabBar.jsx
@@ -6,7 +6,6 @@ import './TabBar-style.css';
 import Projects from './projects-tab/Projects.jsx';
 import Home from './home-tab/Home.jsx';
 import Contact from './contact-tab/Contact.jsx';
-import kellymills from './assets/kellymills.png';
 
 function TabBar() {
     const defaultSelectedId = "default-selected-tab";
@@ -14,11 +13,9 @@ function TabBar() {
         <div className="wrapper">
         <Ariakit.TabProvider defaultSelectedId={ defaultSelectedId }>
             <Ariakit.TabList className="tab-list" aria-label="Navigation Tabs">
-                <Ariakit.Tab className="tab">projects</Ariakit.Tab>
-                <Ariakit.Tab className={ "tab home-tab" } id={ defaultSelectedId }>
-                    <img className="tab-img" src={ kellymills } alt="Kelly Mills - Home Tab"></img>
-                </Ariakit.Tab>
-                <Ariakit.Tab className="tab">contact</Ariakit.Tab>
+                <Ariakit.Tab className="tab">Projects</Ariakit.Tab>
+                <Ariakit.Tab className={ "tab home-tab" } id={ defaultSelectedId }>Kelly Mills</Ariakit.Tab>
+                <Ariakit.Tab className="tab">Contact</Ariakit.Tab>
             </Ariakit.TabList>
             <div className="panels">
             <Ariakit.TabPanel>
