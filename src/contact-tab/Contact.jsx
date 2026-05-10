79d550b2d857bd00933e075eaf5909dc3fc263ba
Kelly Ervin Mills <17459119+ervinkellym@users.noreply.github.com>
2026-05-09 21:24:12 -0400

    Remove calendly link, update contact button link to mailto

--- src/contact-tab/Contact.jsx
+++ src/contact-tab/Contact.jsx
@@ -3,16 +3,15 @@ function Contact() {
         <div className="tab-wrapper">
             <div className="text-block center-text">
                 <p>The best way to reach me is via email at ervinkellym@gmail.com.</p>
-                <p>If you want to chat then please book a time using my calendly link below!</p>
             </div>
             <div>
                 <a 
                     className="link hop-up-on-hover" 
                     target="_blank" 
                     rel="noopener noreferrer" 
-                    href="https://calendly.com/ervinkellym"
+                    href="mailto:ervinkellym@gmail.com"
                 >
-                    Schedule a Meeting
+                    Send an email ✉️
                 </a>
             </div>
             </div>
